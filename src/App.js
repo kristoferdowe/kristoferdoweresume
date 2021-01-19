553972beed902caeb23eeb36cefd07b68a55abd7
kristoferdowe <kristoferdowe@gmail.com>
2021-01-19 12:47:30 -0500

    first comit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React,  {Component} from 'react';
+import './App.css';
+import { Layout, Header, HeaderRow, Textfield, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      
+     
+    <div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" waterfall>
+            <HeaderRow title="Kristofer Dowe">
+                <Textfield
+                    value=""
+                    onChange={() => {}}
+                    label="Search2"
+                    expandable
+                    expandableIcon="search"
+                />
+            </HeaderRow>
+            <HeaderRow>
+                <Navigation>
+                <Link to="/">Home</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+                </Navigation>
+            </HeaderRow>
+        </Header>
+        <Drawer title="Kristofer Dowe">
+            <Navigation>
+                <Link to="/">Home</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
+      
+    )
+  }
+}
+
+  
+export default App;
