656dc127110d9db8cdfa85a8fae756784b87675e
kristoferdowe <kristoferdowe@gmail.com>
2021-01-20 16:50:32 -0500

    updated projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -18,7 +18,7 @@ class Projects extends Component {
 
             <Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
                 <CardTitle style={{color: '#ffff', height: '176 px', background: 'url(https://venturebeat.com/wp-content/uploads/2017/05/dayz.jpg?fit=1920%2C1080&strip=all) center / cover'}}> SurvivaList</CardTitle>
-                <CardText>Parse DayZ local file to instantaneously share your loadout with friends</CardText>
+                <CardText>Parse DayZ local files to instantaneously share your loadout with friends</CardText>
                 <CardActions border>
                     <Button colored href="https://github.com/kristoferdowe/dayzinventorymanager" rel="noopener noreferrer" target="_blank"> Github </Button>
                 </CardActions>
@@ -40,10 +40,10 @@ class Projects extends Component {
 
     <div className='project-grid'>
             <Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
-                <CardTitle style={{color: '#ffff', height: '176 px', background: 'url(https://venturebeat.com/wp-content/uploads/2017/05/dayz.jpg?fit=1920%2C1080&strip=all) center / cover'}}> DayZ Inventory Manager</CardTitle>
-                <CardText>Parse DayZ local file to instantaneously share your loadout with friends</CardText>
+                <CardTitle style={{color: '#ffff', height: '176 px', background: 'url(https://images.unsplash.com/photo-1594445849191-e21cd1ab1ec7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80) center / cover'}}> Quiz App</CardTitle>
+                <CardText>A Simple Quiz App Using jQuery</CardText>
                 <CardActions border>
-                    <Button colored href="https://github.com/kristoferdowe/dayzinventorymanager" rel="noopener noreferrer" target="_blank"> Github </Button>
+                    <Button colored href="https://github.com/kristoferdowe/quizapp" rel="noopener noreferrer" target="_blank"> Github </Button>
                 </CardActions>
                 <CardMenu style={{color: '#fff'}}>
                 <IconButton name='share'/>
@@ -62,10 +62,10 @@ class Projects extends Component {
 
 
             <Card shadow={5} style={{ minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#ffff', height: '176 px', background: 'url(https://venturebeat.com/wp-content/uploads/2017/05/dayz.jpg?fit=1920%2C1080&strip=all) center / cover'}}> DayZ Inventory Manager</CardTitle>
-            <CardText>Parse DayZ local file to instantaneously share your loadout with friends</CardText>
+            <CardTitle style={{color: '#ffff', height: '176 px', background: 'url(https://images.unsplash.com/photo-1594445849191-e21cd1ab1ec7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80) center / cover'}}> Scrambler</CardTitle>
+            <CardText>A simple, customizable word-scrambler for sending and decoding secret messages</CardText>
             <CardActions border>
-                <Button colored href="https://github.com/kristoferdowe/dayzinventorymanager" rel="noopener noreferrer" target="_blank"> Github </Button>
+                <Button colored href="https://repl.it/@kristoferdowe/Scramble#index.html" rel="noopener noreferrer" target="_blank"> Github </Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
             <IconButton name='share'/>
@@ -84,7 +84,9 @@ render() {
         return (
             <div className= 'category-tabs'>
                 <Tabs activeTab = {this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
-                <Tab>Passion Projects</Tab>
+                <Tab> SurvivaList</Tab>
+                <Tab> Quiz App</Tab>
+                <Tab> Word Scrambler</Tab>
                 
                 </Tabs>
            
@@ -92,6 +94,7 @@ render() {
                 <Grid>
                     <Cell col={12}>
                         <div className = 'content-projects'>{this.toggleCategories()}</div>
+                       
                     </Cell>
                 </Grid>
             </div>
